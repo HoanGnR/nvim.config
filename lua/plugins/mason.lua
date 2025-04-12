33b4ac5790a735874cf273f79670b9e2c9bd1925
hoannd23 <hoannguyendac23@gmail.com>
2025-04-13 00:33:51 +0700

    fix: remove ensure_installed for mason.nvim, manually install on installing Nvim.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,26 +1,15 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		opts = {
-			ensure_installed = {
-				-- Go tools
-				"delve", -- Debugger
-				"gofumpt", -- Stricter gofmt
-				"goimports", -- (Optional) For imports management
-				"golines", -- (Optional) For line wrapping
-				"gomodifytags", -- (Optional) For struct tag management
-				"gotests", -- (Optional) For generating tests
-				"impl", -- (Optional) For interface implementation
-			},
-		},
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		automatic_installation = true,
-		ensure_installed = {
-			"elp",
-			"gopls",
-		},
-	},
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    automatic_installation = true,
+    ensure_installed = {
+      "elp",
+      "gopls",
+    },
+  },
 }
